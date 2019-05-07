71f5d4fc6cb0b6ad92b20d40ca2a0aed1bd38483
Psqrt <guy.tsang@etudiant.univ-rennes1.fr>
2019-05-07 13:22:03 +0200

    carto opérationnelle

--- map_0506.R
+++ map_0506.R
@@ -10,18 +10,48 @@ library(tidyverse)
 # Importations
 ###################################################################################################
 
-# DONNEES - CARTE
-data_map2003 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2003_4326_LEVL_2.geojson", what = "sp")
-data_map2006 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2006_4326_LEVL_2.geojson", what = "sp")
-data_map2010 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2010_4326_LEVL_2.geojson", what = "sp")
-data_map2013 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2013_4326_LEVL_2.geojson", what = "sp")
-data_map2016 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2016_4326_LEVL_2.geojson", what = "sp")
+nuts = 0
+annee_enquete = 2005
+if (annee_enquete < 2006){
+    annee_carte = 2003
+} else if (annee_enquete >= 2006 & annee_enquete < 2010){
+    annee_carte = 2006
+} else if (annee_enquete >= 2010 & annee_enquete < 2013){
+    annee_carte = 2010
+} else if (annee_enquete >= 2013 & annee_enquete < 2016){
+    annee_carte = 2013
+} else {
+    annee_carte = 2016
+}
+
+
+# # DONNEES - CARTE NUTS2
+# data_map2003_nuts2 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2003_4326_LEVL_2.geojson", what = "sp")
+# data_map2006_nuts2 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2006_4326_LEVL_2.geojson", what = "sp")
+# data_map2010_nuts2 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2010_4326_LEVL_2.geojson", what = "sp")
+# data_map2013_nuts2 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2013_4326_LEVL_2.geojson", what = "sp")
+# data_map2016_nuts2 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2016_4326_LEVL_2.geojson", what = "sp")
+# # DONNEES - CARTE NUTS1
+# data_map2003_nuts1 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2003_4326_LEVL_1.geojson", what = "sp")
+# data_map2006_nuts1 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2006_4326_LEVL_1.geojson", what = "sp")
+# data_map2010_nuts1 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2010_4326_LEVL_1.geojson", what = "sp")
+# data_map2013_nuts1 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2013_4326_LEVL_1.geojson", what = "sp")
+# data_map2016_nuts1 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2016_4326_LEVL_1.geojson", what = "sp")
+# # DONNEES - CARTE NUTS0
+# data_map2003_nuts0 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2003_4326_LEVL_0.geojson", what = "sp")
+# data_map2006_nuts0 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2006_4326_LEVL_0.geojson", what = "sp")
+# data_map2010_nuts0 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2010_4326_LEVL_0.geojson", what = "sp")
+# data_map2013_nuts0 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2013_4326_LEVL_0.geojson", what = "sp")
+# data_map2016_nuts0 = geojsonio::geojson_read("./data/map/NUTS_RG_01M_2016_4326_LEVL_0.geojson", what = "sp")
+
 
 # DONNEES - ENQUETE
 liste_nom_fichier = list.files("./data/enquete/")
+liste_nom_fichier = liste_nom_fichier[grep(annee_enquete, liste_nom_fichier)]
 liste_nom_fichier = substr(liste_nom_fichier, 1, nchar(liste_nom_fichier)-4)
 
-type_fichier = c("h", "d", "p", "r")
+# type_fichier = c("h", "d", "p", "r")
+type_fichier = c("h", "d")
 
 df_h = data.frame()
 df_d = data.frame()
@@ -56,13 +86,22 @@ for (fichier in liste_nom_fichier){
 
 # DONNEES - DICTIONNAIRE
 
-dico_enquete = read.csv(file = "./data/dictionnaire_codage_apres_2010.csv",
+dico_enquete_avant_2010 = read.csv(file = "./data/dictionnaire_codage_avant_2010.csv",
                         sep = ",",
                         header = T,
                         stringsAsFactors = F)
 
-dico_enquete = setNames(dico_enquete$code_carte, 
-                        dico_enquete$code_enquete)
+dico_enquete_avant_2010 = setNames(dico_enquete_avant_2010$code_carte, 
+                                   dico_enquete_avant_2010$code_enquete)
+
+
+dico_enquete_apres_2010 = read.csv(file = "./data/dictionnaire_codage_apres_2010.csv",
+                        sep = ",",
+                        header = T,
+                        stringsAsFactors = F)
+
+dico_enquete_apres_2010 = setNames(dico_enquete_apres_2010$code_carte, 
+                                   dico_enquete_apres_2010$code_enquete)
 
 
 dico_enquete_croatie_apres_2010 = read.csv(file = "./data/dictionnaire_codage_croatie_apres_2010.csv",
@@ -73,6 +112,16 @@ dico_enquete_croatie_apres_2010 = read.csv(file = "./data/dictionnaire_codage_cr
 dico_enquete_croatie_apres_2010 = setNames(dico_enquete_croatie_apres_2010$code_carte, 
                                            dico_enquete_croatie_apres_2010$code_enquete)
 
+
+dico_enquete_uk_apres_2013 = read.csv(file = "./data/dictionnaire_codage_uk_apres_2013.csv",
+                                      sep = ",",
+                                      header = T,
+                                      stringsAsFactors = F)
+
+dico_enquete_uk_apres_2013 = setNames(dico_enquete_uk_apres_2013$code_carte, 
+                                      dico_enquete_uk_apres_2013$code_enquete)
+
+
 ###################################################################################################
 # Fusion - traitement (Selection des variables)
 ###################################################################################################
@@ -81,10 +130,16 @@ dico_enquete_croatie_apres_2010 = setNames(dico_enquete_croatie_apres_2010$code_
 df_t = df_h %>% 
     inner_join(df_d, by = c("HB030" = "DB030", "HB020" = "DB020"))
 
-# codage_croatie_apres_2010 = c("HR01" = "HR04",
-#                               "HR02" = "HR04")
 
-df_t$DB040 = recode(df_t$DB040, !!!dico_enquete_croatie_apres_2010)
+### Recodage des regions pour etre conforme a la norme en vigueur #################################
+if (nuts == 2){
+    if (annee_carte >= 2010) 
+        print("XX1")
+        df_t$DB040 = recode(df_t$DB040, !!!dico_enquete_croatie_apres_2010)
+    if (annee_carte >= 2013)
+        print("XX2")
+        df_t$DB040 = recode(df_t$DB040, !!!dico_enquete_uk_apres_2013)
+}
 
 # TEST / Nombre d'observations pour chaque region de l'Europe
 nb_obs_per_region = df_t %>% 
@@ -152,35 +207,60 @@ data2 = data2 %>%
            leaking = if_else(leaking == 2, 0, 1))
 
 # Calcul des moyennes par region
-moy_region = data2 %>% 
-    group_by(region) %>% 
-    summarise_all(mean, na.rm = T) %>% 
-    # mutate(region = as.character(region)) %>% 
-    filter(region != "")
-
-toto = data2 %>% 
-    group_by(region) %>% 
-    summarise_all(mean, na.rm = T) %>% 
-    # mutate(region = as.character(region)) %>% 
-    filter(region != "")
+if (nuts == 2){
+    print("XX3")
+    moy_region = data2 %>% 
+        group_by(region) %>% 
+        summarise_all(mean, na.rm = T) %>% 
+        # mutate(region = as.character(region)) %>% 
+        filter(region != "")
+} else if (nuts <= 1){
+    if (annee_carte < 2010){
+        print("XX4")
+        moy_region = data2 %>% 
+            mutate(region = recode(region, !!!dico_enquete_avant_2010))
+    } else if (annee_carte >= 2010 & annee_carte < 2013){
+        print("XX5")
+        moy_region = data2 %>% 
+            mutate(region = recode(region, !!!dico_enquete_apres_2010))
+    }
+    print("XXXXXXXXXXXXXXXXXXXXXXX")
+    moy_region = data2 %>% 
+        mutate(region = substr(region, 1, nuts + 2)) %>%
+        group_by(region) %>% 
+        summarise_all(mean, na.rm = T) %>% 
+        filter(region != "")
+}
 
 ###################################################################################################
 # Gestion des erreurs des données enquête
 ###################################################################################################
 
-moy_region$region = recode(moy_region$region, !!!dico_enquete)
-
-
-
-
-
+if (nuts == 2){
+    if (annee_carte >= 2010){
+        print("XX6")
+        moy_region$region = recode(moy_region$region, !!!dico_enquete_apres_2010)
+    } else if (annee_carte < 2010){
+        print("XX7")
+        moy_region$region = recode(moy_region$region, !!!dico_enquete_avant_2010)
+    }
+} else if (nuts == 0){
+    if (annee_carte < 2010){
+        print("ZZZZZZZZZZZZZZZZZ")
+        moy_region = moy_region %>% 
+            mutate(region = recode(region, !!!c("EL" = "GR")))
+    }
+}
 
 ###################################################################################################
 # Récupération des régions de la carte
 ###################################################################################################
 
 # liste des identifiants nuts2 de la carte
-liste_ids = data.frame("region" = data_map2013$NUTS_ID)
+
+choix_carte = paste0("data_map", annee_carte, "_nuts", nuts, sep = "")
+liste_ids = data.frame("region" = get(choix_carte)$NUTS_ID)
+    
 # conversion en char pour merger
 liste_ids[, "region"] = as.character(liste_ids[, "region"])
 
@@ -196,9 +276,35 @@ moyenne_region = liste_ids %>%
 # 
 # data_agg %>% nrow
 
+###################################################################################################
+# Gestion du cas de Londres en 2013 (2 valeurs pour 5 régions)
+###################################################################################################
 
-
-
+if (nuts == 2 & annee_carte >= 2013){
+    print("XX8")
+    liste_ids_londres = liste_ids %>% 
+        data.frame %>% 
+        rownames_to_column() %>% 
+        filter(substr(region, 1, 3) == "UKI")
+    
+    nb_region_londres = liste_ids_londres %>% nrow
+    
+    moyenne_londres = data2 %>%
+        group_by(region) %>%
+        summarise_all(mean, na.rm = T) %>%
+        filter(region == "UKIX") %>% 
+        slice(rep(1, each = nb_region_londres)) %>% 
+        mutate(region = liste_ids_londres$region) %>% 
+        left_join(liste_ids_londres, by = c("region"))
+    
+    moyenne_region = moyenne_region %>%
+        rownames_to_column() %>%
+        anti_join(moyenne_londres, by = c("region")) %>%
+        bind_rows(moyenne_londres) %>%
+        mutate(rowname = as.integer(rowname)) %>%
+        arrange(rowname) %>% 
+        select(-rowname)
+}
 
 ###################################################################################################
 # Gestion des cas NUTS1
@@ -257,7 +363,7 @@ moyenne_region = liste_ids %>%
 pal <- colorNumeric("viridis", NULL, na.color = "red", alpha = T)
 
 # Carte leaflet
-leaflet(data_map2013) %>% 
+leaflet(get(choix_carte)) %>% 
     addTiles(urlTemplate = "//{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png") %>% 
     addPolygons(stroke = FALSE,
                 smoothFactor = 0.3,
