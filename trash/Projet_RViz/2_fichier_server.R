d44ec61dd40dd2e6c801f047b59f0094911189c1
Psqrt <guy.tsang@etudiant.univ-rennes1.fr>
2019-06-08 12:44:42 +0200

    cleaning

--- trash/Projet_RViz/2_fichier_server.R
+++
@@ -1,9 +0,0 @@
-contenu_server = shinyServer(
-    function(input, output, session) {
-        
-        source("./4_server_joueurs.R", local = T)$value
-        source("./4_server_reseau.R", local = T)$value
-        source("./4_server_carto.R", local = T)$value
-        
-    }
-)
